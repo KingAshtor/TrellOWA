452dfdb5103a4a8e7653e9b3cd82c18765e6b48f
Ashton <Ashto2020303@live.ytech.edu>
2019-11-07 14:39:42 -0500

    aslk;djf;lkasdkjaslj;fsdsdasddfsadfasfsdfsdsdfa

--- saveList.js
+++ saveList.js
@@ -1,20 +1,18 @@
-var printedData
+var printedData = "BLANK"
 
 function listSave() {
-  let printedData = document.getElementById('sortable1').children;
+  printedData = document.getElementById('sortable1').children;
   console.log(printedData);
+}
 
-  // Requiring fs module in which
-  // writeFile function is defined.
-  const fs = require('fs')
-
-  // Data which will write in a file.
-  let data = "Learning how to write in a file."
-
-  // Write data in 'Output.txt' .
-  fs.writeFile('Output.txt', data, (err) => {
-
-    // In case of a error throw err.
-    if (err) throw err;
-  })
+function listLoad() {
+  // let printedData = document.getElementById('sortable1').children;
+  // console.log(printedData);
+  var ul = document.getElementById('sortable1'); // sending point, id sortable 1
+  var candidate = document.getElementById('candidate'); // sets candidate equal to the inputted name
+  var li = document.createElement('li'); //sets variable li equal to li
+  var desc = document.getElementById('desc') //sets variable desc equal to desc
+  li.appendChild(document.createTextNode(printedData)); //creates the li using both candidate's value and desc's value, adding them together and putting them in parenthesis
+  ul.appendChild(li); //adds the list item to the list
+  console.log(printedData.HTMLCollection);
 }
